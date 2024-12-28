6e9b08c10e76de1f3a1bcf9d4cf23a6fa22e36e2
Kaameshwaran V <727721euec062@skcet.ac.in>
2024-12-29 01:24:12 +0530

    show issue details

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import { Button } from '@radix-ui/themes';
 import { Table } from '@radix-ui/themes';
-import Link from 'next/link';
-import { PlusCircle, Pointer} from 'lucide-react';
 import IssueStatusBadge from '../components/IssueStatusBadge';
 import prisma from '@/prisma/client';
+// import delay from 'delay'
+import IssueActions from './IssueActions';
+import Link from 'next/link';
 
 const Issues = async () => {
   const issues = await prisma.issue.findMany()
-
+  // await delay(2000)
   return (
     <div className="p-6">
-      <div className="flex items-center justify-between mb-6">
-        <h1 className="text-2xl font-bold">Issue Tracker</h1>
-        <Link href="./issues/new" passHref>
-          <Button style={{cursor : 'pointer'}}>
-            <PlusCircle size={18} />
-            New Issue
-          </Button>
-        </Link>
-      </div>
-
+      <IssueActions/>
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
@@ -33,7 +24,11 @@ const Issues = async () => {
         <Table.Body>
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
-              <Table.Cell>{issue.title}</Table.Cell>
+              <Table.Cell>
+                <Link href={`/issues/${issue.id}`}>
+                  {issue.title}
+                </Link>
+              </Table.Cell>
               <Table.Cell>
                 <IssueStatusBadge status={issue.status} />
               </Table.Cell>
