6e85bbb119a59c9f32ab24e7204806115f27458d
Kaameshwaran V <727721euec062@skcet.ac.in>
2024-12-28 16:55:14 +0530

    show the issues from db

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -18,19 +18,20 @@ const NewIssuePage = () => {
     });
     const router = useRouter()
     const[isSubmitting, setIsSubmitting] = useState(false);
+    const submit = handleSubmit(async (data) => {
+        try {
+            await axios.post('/api/issues', data);
+            router.push('/issues');
+            setIsSubmitting(true);
+        } catch (error) {
+            setIsSubmitting(false);
+        }
+    })
 
   return (
     <div className='max-w-xl p-6'>
     <form
-        onSubmit={handleSubmit(async (data) => {
-            try {
-                await axios.post('/api/issues', data);
-                router.push('/issues');
-                setIsSubmitting(true);
-            } catch (error) {
-                setIsSubmitting(false);
-            }
-        })}
+        onSubmit={submit}
         className='space-y-6'>
         
         <div className='flex flex-col'>
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,11 +1,43 @@
 
 import React from 'react'
-import { Button } from '@radix-ui/themes'
+import { Button, Table } from '@radix-ui/themes'
 import Link from 'next/link'
+import prisma from '@/prisma/client'
 
-const Issues = () => {
+const Issues = async () => {
+  const issues = await prisma.issue.findMany();
   return (
-    <div ><Link href={'./issues/new'}><Button>New Issue</Button></Link></div>
+    <div >
+      <div className='mb-5'>
+        <Link href={'./issues/new'}><Button>New Issue</Button></Link>
+      </div>
+      <div className="max-w-full overflow-x-auto">
+      <Table.Root 
+        variant="surface" 
+        className="w-auto border border-gray-300 rounded-lg shadow-sm"
+      >
+      <Table.Header className="text-white">
+        <Table.Row>
+          <Table.ColumnHeaderCell className="py-3 px-4 text-left">Issues</Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell className="py-3 px-4 text-left">Status</Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell className="py-3 px-4 text-left">Created At</Table.ColumnHeaderCell>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {issues.map((issue) => (
+          <Table.Row
+            key={issue.id}
+            className="hover:bg-blue-50 even:bg-gray-100"
+          >
+            <Table.Cell className="py-2 px-4">{issue.title}</Table.Cell>
+            <Table.Cell className="py-2 px-4">{issue.status}</Table.Cell>
+            <Table.Cell className="py-2 px-4">{issue.createdAt.toDateString()}</Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+      </Table.Root>
+    </div>
+    </div>
   )
 }
 
