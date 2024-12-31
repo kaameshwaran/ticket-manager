aa55722dd2990f9a48a2fc3a6c9f6b24af3c55b5
Kaameshwaran V <727721euec062@skcet.ac.in>
2024-12-31 12:56:50 +0530

    Add confirmation dialog to delete ticket

--- app/issues/[id]/edit/DeleteIssueButton.tsx
+++ app/issues/[id]/edit/DeleteIssueButton.tsx
@@ -1,13 +1,35 @@
-import { Button } from '@radix-ui/themes'
+'use client'
+import {AlertDialog, Button, Flex } from '@radix-ui/themes'
 import { Trash } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
 
 const DeleteIssueButton = ({ issueId }: {issueId: number}) => {
   return (
-    <Button color='red' style={{cursor: 'pointer'}}>
-        <Trash size={18}/>Delete
-    </Button>
+    <AlertDialog.Root>
+		<AlertDialog.Trigger>
+			<Button color='red' style={{cursor: 'pointer'}}>
+                <Trash size={18}/>Delete
+            </Button>
+		</AlertDialog.Trigger>
+			<AlertDialog.Content>
+				<AlertDialog.Title>
+					Are you absolutely sure?
+				</AlertDialog.Title>
+				<AlertDialog.Description>
+					This action cannot be undone. This will permanently delete your
+					ticket from our servers.
+				</AlertDialog.Description>
+                <Flex mt={'2'} gap={'3'}>
+					<AlertDialog.Cancel>
+						<Button style={{cursor: 'pointer'}} color='gray'>Cancel</Button>
+					</AlertDialog.Cancel>
+					<AlertDialog.Action>
+						<Button style={{cursor: 'pointer'}} color='red'>Yes, delete</Button>
+					</AlertDialog.Action>
+                </Flex>
+			</AlertDialog.Content>
+	</AlertDialog.Root>
   )
 }
 
