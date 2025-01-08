111a02f72de51bd5106bfc6dfaa36061eca524f2
Kaameshwaran V <727721euec062@skcet.ac.in>
2025-01-08 15:44:40 +0530

    feature: sort issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,26 +1,56 @@
 import { Flex, Table } from '@radix-ui/themes';
 import prisma from '@/prisma/client';
 import {IssueStatusBadge, Link, IssueActions} from '@/app/components';
-import { Status } from '@prisma/client';
+import NextLink from 'next/link'
+import { Issue, Status } from '@prisma/client';
+import { ArrowUpDown } from 'lucide-react';
 
-const Issues = async ({ searchParams }: { searchParams : {status: Status}}) => {
+const Issues = async ({ searchParams }: { searchParams : {status: Status, orderBy: keyof Issue, orderDirection: 'asc' | 'desc' }}) => {
+  const columns: {label: String, value: keyof Issue}[] =[
+    {label: 'Issue', value: 'title'},
+    {label: 'Status', value: 'status'},
+    {label: 'Created', value: 'createdAt'},
+  ]
+  
   const statuses = Object.values(Status);
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
     : undefined;
 
+  const orderDirection = searchParams.orderDirection || 'asc';
+  const orderBy = columns.map(column => column.value).includes(searchParams.orderBy)
+  ? { [searchParams.orderBy]: orderDirection }
+  : undefined
+
   const issues = await prisma.issue.findMany({
-    where: { status }
+    where: { status },
+    orderBy
   })
+
+  const nextOrderDirection = orderDirection === 'asc' ? 'desc' : 'asc';
+
   return (
     <Flex direction={'column'} p="2">
-      <IssueActions/>
+      <IssueActions currentStatus={status === undefined ? "All" : status}/>
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell>Issues</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Status</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Created</Table.ColumnHeaderCell>
+            {columns.map(({ label, value }) => (
+              <Table.ColumnHeaderCell key={value}>
+                  <NextLink href={{
+                    query: {
+                      ...searchParams,
+                      orderBy: value,
+                        orderDirection: nextOrderDirection,
+                      }
+                  }}>
+                    {label}
+                  </NextLink>
+                  {value === searchParams.orderBy &&
+                    <ArrowUpDown size={17} className='inline'/>
+                  }
+              </Table.ColumnHeaderCell>
+            ))}
           </Table.Row>
         </Table.Header>
 
