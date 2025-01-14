9faac67bda5127c3b3c0cfc55ceaf3056776598f
Kaameshwaran V <727721euec062@skcet.ac.in>
2025-01-15 02:12:10 +0530

    implement pagination

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { Button, Flex } from '@radix-ui/themes';
+import { useRouter, useSearchParams } from 'next/navigation';
 import React from 'react';
 import {
   AiFillBackward,
@@ -15,21 +16,33 @@ interface Props {
 }
 
 const Pagination = ({ totalItem, pageSize, currentPage }: Props) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const handlePageChange = (page: number) => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set('page', page.toString());
+    router.push('?' + params.toString());
+  };
+
   const totalPage = Math.ceil(totalItem / pageSize);
+  if (totalPage <= 1) return null;
   return (
     <>
       <Flex align={'center'} justify={'center'} gap={'2'}>
-        <Button 
-        color="gray" 
-        variant="soft" 
-        disabled={currentPage === 1}
+        <Button
+          color="gray"
+          variant="soft"
+          disabled={currentPage === 1}
+          onClick={() => handlePageChange(1)}
         >
           <AiFillBackward />
         </Button>
         <Button
-        color="gray" 
-        variant="soft" 
-        disabled={currentPage === 1}
+          color="gray"
+          variant="soft"
+          disabled={currentPage === 1}
+          onClick={() => handlePageChange(currentPage - 1)}
         >
           <AiFillCaretLeft />
         </Button>
@@ -40,6 +53,7 @@ const Pagination = ({ totalItem, pageSize, currentPage }: Props) => {
           color="gray"
           variant="soft"
           disabled={currentPage === totalPage}
+          onClick={() => handlePageChange(currentPage + 1)}
         >
           <AiFillCaretRight />
         </Button>
@@ -47,6 +61,7 @@ const Pagination = ({ totalItem, pageSize, currentPage }: Props) => {
           color="gray"
           variant="soft"
           disabled={currentPage === totalPage}
+          onClick={() => handlePageChange(totalPage)}
         >
           <AiFillForward />
         </Button>
--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@ import Link from 'next/link'
 import React from 'react'
 import Pagination from './components/Pagination'
 
-const page = () => {
+const page = ({searchParams}: { searchParams: { page: string } }) => {
   return (
     <>
     <div className='text-black ml-10'>Home</div>
-    <Pagination totalItem={120} currentPage={1} pageSize={10} />
+    <Pagination totalItem={120} currentPage={parseInt(searchParams.page)} pageSize={10} />
     </>
   )
 }
